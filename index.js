1ae81848d7de44b278b0d92b74de2c4774bcc335
alinadovgan <101360006+alinadovgan@users.noreply.github.com>
2022-03-20 11:34:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+function task1() {
+let admin;
+let user = "Anna";
+admin = user;
+alert = (user);
+}
+
+function task2() {
+    const name = 'ivan'
+    const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1)
+}
+
+function task3 () {
+if (age > 18) {
+    greeting = "Adult"
+}
+else {
+    greeting = "Child"
+}                     //Boolean - только правда или ложь 
+
+
+var box = null; foo
+'null' // переменой box существует, но она не имеет ни типа ни значения
+
+var x;
+if (x === undefined) // пееменая x не определена 
+
+var y;
+y=16; // числа 
+
+
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+alert (id1 == id2); //false, символ, даже если описание будет одинаковым символы будут разными 
+
+let bigInt = 999879889765n;
+console.log(bigInt);
+999879889765n
+console.log(typeof bigInt);
+"bigint"  //BigInt представление больших целых чисел
+
+
+function task4 () {
+    let a = '';
+    let b = 'bbb';
+    let c = false;
+    let test1 = !b.length === c; // true
+    let test2 = a.length > b.length; // false 
+    let test3 = !a.length && !b.length; //false. && коньюкция, для набора типа Boolean
+    let test4 = a === c; // false
+    
+}
+
+
+
+
+
+
+
+
+
+
+}
